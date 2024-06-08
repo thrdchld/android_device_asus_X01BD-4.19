31cb9c6bfae10893422aa6a321fd2b6aacb570ce
thrdchld <109644096+thrdchld@users.noreply.github.com>
2024-06-08 17:13:00 +0700

    Update lineage_X01BD.mk

--- lineage_X01BD.mk
+++ lineage_X01BD.mk
@@ -12,19 +12,34 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 # Inherit some common LineageOS stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# RisingOS variables
+RISING_MAINTAINER := thrdchld
+
+
+# Bootanimation
+TARGET_BOOT_ANIMATION_RES := 1080
+
 # Inherit from X01BD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+# Gapps Config
+WITH_GMS := false
+RISING_PACKAGE_TYPE := "VANILLA_AOSP"
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_X01BD
 PRODUCT_DEVICE := X01BD
 PRODUCT_BRAND := asus
-PRODUCT_MODEL := Zenfone Max Pro M2
+PRODUCT_MODEL := ASUS_X01BDA
 PRODUCT_MANUFACTURER := asus
 
 PRODUCT_GMS_CLIENTID_BASE := android-asus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_DEVICE=ASUS_X01BD \
-    PRODUCT_NAME=WW_X01BD
+    PRODUCT_DEVICE=ASUS_X01BD_1 \
+    PRODUCT_NAME=WW_X01BD \
+    RISING_MAINTAINER := thrdchld \
+    PRIVATE_BUILD_DESC="sdm660_64-user 10 QKQ1 73 release-keys"
 
+# Build fingerprint
+BUILD_FINGERPRINT := "asus/WW_X01BD/ASUS_X01BD_1:10/QKQ1/17.2018.2012.434-20201203:user/release-keys"
