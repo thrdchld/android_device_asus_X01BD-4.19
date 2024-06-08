20ef41fdf99e3c6822c4dd3888014080f54b1bb5
thrdchld <109644096+thrdchld@users.noreply.github.com>
2024-06-08 17:05:38 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,4 +8,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_X01BD.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_X01BD-ap1a-userdebug
+    lineage_X01BD-userdebug \
+    lineage_X01BD-user \
+    lineage_X01BD-eng
