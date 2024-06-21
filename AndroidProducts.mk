bbc623224fbd78ff305d3001cc1ee9db570c2aab
thrdchld <109644096+thrdchld@users.noreply.github.com>
2024-06-21 10:34:26 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,7 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_X01BD.mk
+    $(LOCAL_DIR)/yaap_X01BD.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_X01BD-ap1a-userdebug
+    yaap_X01BD-user \
+    yaap_X01BD-userdebug \
+    yaap_X01BD-eng
