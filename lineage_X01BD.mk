ee33dd65f0f2a334ded1841537b5092ab8024142
thrdchld <109644096+thrdchld@users.noreply.github.com>
2024-07-09 11:23:18 +0700

    Update lineage_X01BD.mk

--- lineage_X01BD.mk
+++ lineage_X01BD.mk
@@ -26,5 +26,8 @@ PRODUCT_GMS_CLIENTID_BASE := android-asus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE=ASUS_X01BD \
-    PRODUCT_NAME=WW_X01BD
+    PRODUCT_NAME=WW_X01BD \
+    PRIVATE_BUILD_DESC="walleye-user 8.1.0 OPM1.171019.011 4448085 release-keys"
 
+# Build fingerprint
+BUILD_FINGERPRINT := "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys"
